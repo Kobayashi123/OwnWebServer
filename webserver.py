ede9d00569a6e2f288728b2eb5336e6db230e057
Kobayashi123 <g2053417@cc.kyoto-su.ac.jp>
2022-11-29 16:33:43 +0900

    del: tcpserver.py, add: webserver.py

--- webserver.py
+++ webserver.py
@@ -12,7 +12,7 @@ __date__ = '2022/11/29 (Created: 2022/11/11)'
 import socket
 from datetime import datetime
 
-class TCPserver:
+class WebServer:
 	"""
 	Webサーバーを表すクラス
 	"""
@@ -70,7 +70,7 @@ def main():
 	Pythonファイルを生成するメイン（main）プログラムです。
 	常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
 	"""
-	server = TCPserver()
+	server = WebServer()
 	server.serve()
 
 if __name__ == '__main__':  # このスクリプトファイルが直接実行されたときだけ、以下の部分を実行する。
