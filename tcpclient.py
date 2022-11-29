cb350a304ad7184fa985a12952d1a5d68a864fdd
Kobayashi123 <g2053417@cc.kyoto-su.ac.jp>
2022-11-29 15:54:20 +0900

    fix: space 4 → tab

--- tcpclient.py
+++ tcpclient.py
@@ -12,52 +12,52 @@ __date__ = '2022/11/14 (Created: 2022/11/14)'
 import socket
 
 class TCPclient:
-    """
-    TCP通信を行うクライアントを表すクラス
-    """
-    def __init__(self):
-        """
-        コンストラクタ
-        """
+	"""
+	TCP通信を行うクライアントを表すクラス
+	"""
+	def __init__(self):
+		"""
+		コンストラクタ
+		"""
 
-    def request(self):
-        """
-        サーバーにリクエストを送信する
-        """
-        print("=== クライアントを起動します ===")
+	def request(self):
+		"""
+		サーバーにリクエストを送信する
+		"""
+		print("=== クライアントを起動します ===")
 
-        try:
-            # with socket.socket() as socket:
-            client_socket = socket.socket()
-            client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		try:
+			# with socket.socket() as socket:
+			client_socket = socket.socket()
+			client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            print("=== サーバーと接続します ===")
-            client_socket.connect(("localhost", 8080))
-            print("=== サーバーとの接続が完了しました ===")
+			print("=== サーバーと接続します ===")
+			client_socket.connect(("localhost", 8080))
+			print("=== サーバーとの接続が完了しました ===")
 
-            with open("server_recv.txt", "rb") as f:
-                request = f.read()
+			with open("server_recv.txt", "rb") as f:
+				request = f.read()
 
-            client_socket.send(request)
+			client_socket.send(request)
 
-            response = client_socket.recv(4096)
+			response = client_socket.recv(4096)
 
-            with open("client_recv.txt", "wb") as f:
-                f.write(response)
+			with open("client_recv.txt", "wb") as f:
+				f.write(response)
 
-            client_socket.close()
+			client_socket.close()
 
-        finally:
-            print("=== クライアントを停止します ===")
+		finally:
+			print("=== クライアントを停止します ===")
 
 def main():
-    """
-    Pythonファイルを生成するメイン（main）プログラムです。
-    常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
-    """
-    client = TCPclient()
-    client.request()
+	"""
+	Pythonファイルを生成するメイン（main）プログラムです。
+	常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
+	"""
+	client = TCPclient()
+	client.request()
 
 if __name__ == '__main__':  # このスクリプトファイルが直接実行されたときだけ、以下の部分を実行する。
-    import sys
-    sys.exit(main())
+	import sys
+	sys.exit(main())
--- tcpserver.py
+++ tcpserver.py
@@ -12,57 +12,57 @@ __date__ = '2022/11/11 (Created: 2022/11/11)'
 import socket
 
 class TCPserver:
-    """
-    TCP通信を行うサーバーを表すクラス
-    """
-    def __init__(self):
-        """
-        コンストラクタ
-        """
+	"""
+	TCP通信を行うサーバーを表すクラス
+	"""
+	def __init__(self):
+		"""
+		コンストラクタ
+		"""
 
-    def serve(self):
-        """
-        サーバーを起動する
-        """
-        print("=== TCPサーバーを起動します ===")
+	def serve(self):
+		"""
+		サーバーを起動する
+		"""
+		print("=== TCPサーバーを起動します ===")
 
-        try:
-            server_socket = socket.socket()
-            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		try:
+			server_socket = socket.socket()
+			server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-            server_socket.bind(("localhost", 8080))
-            server_socket.listen(10)
+			server_socket.bind(("localhost", 8080))
+			server_socket.listen(10)
 
-            # server_socket.setblocking(False)
+			# server_socket.setblocking(False)
 
-            print("=== クライアントからの接続を待機します ===")
-            (client_socket, client_address) = server_socket.accept()
-            print(f"=== クライアントとの接続を確立しました client_address: {client_address} ===")
+			print("=== クライアントからの接続を待機します ===")
+			(client_socket, client_address) = server_socket.accept()
+			print(f"=== クライアントとの接続を確立しました client_address: {client_address} ===")
 
-            request = client_socket.recv(4096)
+			request = client_socket.recv(4096)
 
-            with open("server_recv.txt", "wb") as f:
-                f.write(request)
+			with open("server_recv.txt", "wb") as f:
+				f.write(request)
 
-            with open("server_send.txt", "rb") as f:
-                response = f.read()
+			with open("server_send.txt", "rb") as f:
+				response = f.read()
 
-            client_socket.send(response)
+			client_socket.send(response)
 
-            client_socket.close()
+			client_socket.close()
 
-        finally:
-            print("=== TCPサーバーを停止します ===")
+		finally:
+			print("=== TCPサーバーを停止します ===")
 
 def main():
-    """
-    Pythonファイルを生成するメイン（main）プログラムです。
-    常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
-    """
-    server = TCPserver()
-    server.serve()
+	"""
+	Pythonファイルを生成するメイン（main）プログラムです。
+	常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
+	"""
+	server = TCPserver()
+	server.serve()
 
 if __name__ == '__main__':  # このスクリプトファイルが直接実行されたときだけ、以下の部分を実行する。
-    import sys
+	import sys
 
-    sys.exit(main())
+	sys.exit(main())
