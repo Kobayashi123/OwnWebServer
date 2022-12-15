5c85eaaa86244309e3ee2308289ab42cd0d6112a
Kobayashi123 <g2053417@cc.kyoto-su.ac.jp>
2022-12-16 08:17:03 +0900

    clean: creating a template engine

--- moz/views/static.py
+++ moz/views/static.py
@@ -29,11 +29,10 @@ def static(request: HttpRequest) -> HttpResponse:
         with open(static_file_path, "rb") as a_file:
             response_body = a_file.read()
 
-        content_type = ''
-        return HttpResponse(status_code = 200, body = response_body, content_type = content_type)
+        return HttpResponse(body = response_body.decode())
 
     except FileNotFoundError:
         traceback.print_exc()
-        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+        response_body = "<html><body><h1>404 Not Found</h1></body></html>"
         content_type = "text/html"
         return HttpResponse(status_code = 404, body = response_body, content_type = content_type)
--- views.py
+++ views.py
@@ -7,9 +7,8 @@ Python生成プログラム：Pythonファイルを生成します。
 
 __author__ = 'Kobayashi Shun'
 __version__ = '0.0.0'
-__date__ = '2022/12/13 (Created: 2022/12/03)'
+__date__ = '2022/12/16 (Created: 2022/12/03)'
 
-import textwrap
 import urllib.parse
 from datetime import datetime
 from pprint import pformat
@@ -26,75 +25,42 @@ def now(request: HttpRequest) -> HttpResponse:
     context = {"now": datetime.now()}
     html = render("now.html", context)
 
-    response_body = textwrap.dedent(html)
-
-    return HttpResponse(body=response_body)
+    return HttpResponse(body=html)
 
 def show_request(request: HttpRequest) -> HttpResponse:
     """
     リクエストを表示する
     """
-    html = f"""\
-            <html>
-            <body>
-                <h1>Request Line: </h1>
-                <p>{request.method} {request.path} {request.http_version}</p>
-                <h1>Headers: </h1>
-                <pre>{pformat(request.headers)}</pre>
-                <h1>Body: </h1>
-                <pre>{request.body.decode("utf-8", "ignore")}</pre>
-            </body>
-            </html>
-            """
-    response_body = textwrap.dedent(html).encode()
-    content_type = "text/html; charset=utf-8"
+    content = {"request": request,
+               "headers": pformat(request.headers),
+               "body": request.body.decode("utf-8", "ignore"),
+               }
+    html = render("show_request.html", content)
 
-    return HttpResponse(body=response_body, content_type=content_type, status_code=200)
+    return HttpResponse(body=html)
 
 def parameters(request: HttpRequest) -> HttpResponse:
     """
     POSTパラメータ を表示する
     """
-    response_body = b''
-    content_type = ''
-    status_code = 200
+    response_body = ''
 
     if request.method == "GET":
-        response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-        content_type = "text/html; charset=utf-8"
-        status_code = 405
+        response_body = "<html><body><h1>405 Method Not Allowed</h1></body></html>"
+        return HttpResponse(body = response_body, status_code = 405)
 
-    elif request.method == "POST":
-        post_params = urllib.parse.parse_qs(request.body.decode())
-        html = f"""\
-                <html>
-                <body>
-                    <h1>Parameters: </h1>
-                    <pre>{pformat(post_params)}</pre>
-                </body>
-                </html>
-                """
-        response_body = textwrap.dedent(html).encode()
-        content_type = "text/html; charset=utf-8"
-        status_code = 200
+    if request.method == "POST":
+        content = {"params": urllib.parse.parse_qs(request.body.decode())}
+        response_body = render("parameter.html", content)
+        return HttpResponse(body = response_body)
 
-    return HttpResponse(body=response_body, content_type=content_type, status_code=status_code)
+    return HttpResponse(body = response_body)
 
 def user_profile(request: HttpRequest) -> HttpResponse:
     """
     User ID を表示する
     """
-    user_id = request.params["user_id"]
-    html = f"""\
-        <html>
-        <body>
-            <h1>プロフィール</h1>
-            <p>ID: {user_id}</p>
-        </body>
-        </html>
-        """
-    response_body = textwrap.dedent(html).encode()
-    content_type = "text/html; charset=utf-8"
-    status_code = 200
+    content = {"user_id": request.params["user_id"]}
+    html = render("user_profile.html", content)
 
-    return HttpResponse(body=response_body, content_type=content_type, status_code=status_code)
+    return HttpResponse(body=html)
